9582430f1f2fcee0d90154869126d6d38ef55679
Deymeqpyra <deymecop@gmail.com>
2024-10-27 23:12:38 +0200

    Add the sql builder lab 10

---
+++ SqlBuilderProgramLab10/SqlBuilderProgramLab10/Program.cs
@@ -0,0 +1,25 @@
+﻿using SqlBuilderProgramLab10;
+using SqlBuilderProgramLab10.Entity;
+using SqlBuilderProgramLab10.Services;
+
+SqlBuilder sqlBuilder = new PostgreSQLBuilder();
+
+var queryPostgres = sqlBuilder.Select<User>()
+    .OrderBy("RegestrationDate")
+    .OrderByDesc("Name")
+    .Take(10)
+    .Build();
+
+sqlBuilder = new MSSQLBuilder();
+var queryMSSQL = sqlBuilder.Select<User>()
+    .OrderBy("RegestrationDate")
+    .OrderByDesc("Name")
+    .Take(10)
+    .Build();
+
+
+queryPostgres.CommandText();
+queryPostgres.CommandParamets();
+
+queryMSSQL.CommandText();
+queryMSSQL.CommandParamets();
